f95ed858ceb1ef73351a8823ccfd0e76e87f7dc9
Cameron MacFarland <distantcam@gmail.com>
2014-11-26 16:20:50 +0800

    Removing some diagnostic boilerplate

---
+++ src/WeavR.Analysers/Framework/DiagnosticAnalyser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace WeavR.Analysers.Framework
+{
+    public abstract class SymbolDiagnosticAnalyser<T> : DiagnosticAnalyzer where T : ISymbol
+    {
+        private DiagnosticDescriptor descriptor;
+        private SymbolKind symbolKind;
+
+        private SymbolDiagnosticAnalyser()
+        {
+        }
+
+        protected SymbolDiagnosticAnalyser(DiagnosticDescriptor descriptor, SymbolKind symbolKind)
+        {
+            this.descriptor = descriptor;
+            this.symbolKind = symbolKind;
+        }
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(descriptor);
+            }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSymbolAction(AnalyseSymbol, symbolKind);
+        }
+
+        protected virtual void AnalyseSymbol(SymbolAnalysisContext context)
+        {
+            var symbol = (T)context.Symbol;
+            AnalyseSymbol(symbol, d => context.ReportDiagnostic(d));
+        }
+
+        protected abstract void AnalyseSymbol(T symbol, Action<Diagnostic> reportDiagnostic);
+    }
+}
--- src/WeavR.Analysers/ToUppercase/ToUppercaseAnalyser.cs
+++ src/WeavR.Analysers/ToUppercase/ToUppercaseAnalyser.cs
@@ -1,39 +1,28 @@
+using System;
 using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
+using WeavR.Analysers.Framework;
 
 namespace WeavR.Analysers.ToUppercase
 {
     [DiagnosticAnalyzer]
-    public class ToUppercaseAnalyser : DiagnosticAnalyzer
+    public class ToUppercaseAnalyser : SymbolDiagnosticAnalyser<INamedTypeSymbol>
     {
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        public ToUppercaseAnalyser() :base(Descriptors.WV0001, SymbolKind.NamedType)
         {
-            get
-            {
-                return ImmutableArray.Create(Descriptors.WV0001);
-            }
         }
 
-        public override void Initialize(AnalysisContext context)
+        protected override void AnalyseSymbol(INamedTypeSymbol symbol, Action<Diagnostic> reportDiagnostic)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
-        }
-
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
-        {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
             // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            if (symbol.Name.ToCharArray().Any(char.IsLower))
             {
                 // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Descriptors.WV0001, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+                var diagnostic = Diagnostic.Create(Descriptors.WV0001, symbol.Locations[0], symbol.Name);
 
-                context.ReportDiagnostic(diagnostic);
+                reportDiagnostic(diagnostic);
             }
         }
     }
